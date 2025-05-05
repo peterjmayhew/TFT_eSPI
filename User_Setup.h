23a158a388fc5583a9e544ed7b9e120ff0ea2a35
Peter Mayhew <50244222+peterjmayhew@users.noreply.github.com>
2025-05-05 12:17:51 +0100

    Update User_Setup.h

--- User_Setup.h
+++ User_Setup.h
@@ -9,8 +9,8 @@
 //   Note that some sketches are designed for a particular TFT pixel width/height
 
 // User defined information reported by "Read_User_Setup" test & diagnostics example
-#define USER_SETUP_INFO "User_Setup"
-
+//#define USER_SETUP_INFO "User_Setup"
+#include "../../../../include/User_Setup.h"
 // Define to disable all #warnings in library (can be put in User_Setup_Select.h)
 //#define DISABLE_ALL_LIBRARY_WARNINGS
 
